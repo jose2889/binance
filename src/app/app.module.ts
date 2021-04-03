c46ddb676f27e4e1370941fe23967f1f37e5a107
JOSE HERNANDEZ <jhernandez@azurian.com>
2021-04-03 19:20:09 -0300

    identacion de codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import {  LOCALE_ID, NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {ReactiveFormsModule } from'@angular/forms';
-import {FormsModule } from'@angular/forms';
+import { ReactiveFormsModule } from'@angular/forms';
+import { FormsModule } from'@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegistrarOrdenComponent } from './registrar-orden/registrar-orden.component';
